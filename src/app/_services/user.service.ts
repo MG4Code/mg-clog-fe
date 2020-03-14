59aad4bfbc3b81de6a6a0c0103710e68c0b1ad11
georg <georg@noenv.com>
2020-03-14 18:36:09 +0100

    adapt do backend changes

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/user/';
+const AUTH_API = 'http://localhost:8080/clog/v1/user/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
